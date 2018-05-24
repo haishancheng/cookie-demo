bd79fb2fbfc24809caace07619514ec2ad74901e
wangtao <1621829316@qq.com>
2018-05-24 12:35:52 +0800

    最终完成了可以测试cookie，session，cache-control，etag的版本

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var http = require('http')
 var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
+var md5 = require('md5')
 
 if (!port) {
   console.log('请指定端口号好不啦？\node server.js 8888 这样不会吗？')
   process.exit(1)
 }
+
+let sessions = {}
 
 var server = http.createServer(function (request, response) {
   //以  http://localhost:8080/sss?wd=hello&rsv=rsv_spt#5   为例
@@ -17,7 +20,10 @@ var server = http.createServer(function (request, response) {
 
   if (pathName === '/') {
     let string = fs.readFileSync('./index.html', 'utf8')
-    let cookies =  request.headers.cookie.split('; ') // 为了分离出可以识别用户信息的部分
+    let cookies = ''
+    if(request.headers.cookie){
+      cookies =  request.headers.cookie.split('; ') // 可能会有多条cookie，以分号分割
+    }
     let hash = {}
     for(let i =0;i<cookies.length; i++){
       let parts = cookies[i].split('=')
@@ -25,7 +31,11 @@ var server = http.createServer(function (request, response) {
       let value = parts[1]
       hash[key] = value 
     }
-    let email = hash.sign_in_email
+    let mySession = sessions[hash.sessionId]//根据sessionId从sessions中取出email
+    let email 
+    if(mySession){
+      email = mySession.sign_in_email
+    }
     let users = fs.readFileSync('./db/users', 'utf8')
     users = JSON.parse(users)
     let foundUser
@@ -35,7 +45,6 @@ var server = http.createServer(function (request, response) {
         break
       }
     }
-    console.log(foundUser)
     if(foundUser){
       string = string.replace('__password__', foundUser.password)
     }else{
@@ -125,10 +134,7 @@ var server = http.createServer(function (request, response) {
         let value = parts[1]
         hash[key] = decodeURIComponent(value)
       })
-      let {
-        email,
-        password
-      } = hash
+      let {email,password} = hash
       var users = fs.readFileSync('./db/users', 'utf8')
       try {
         users = JSON.parse(users) // []
@@ -143,14 +149,40 @@ var server = http.createServer(function (request, response) {
         }
       }
       if (found) {
-        response.setHeader('Set-Cookie', `sign_in_email=${email}`)
+        // response.setHeader('Set-Cookie', `sign_in_email=${email}; max-age=60`)
+        // response.setHeader('Set-Cookie', `sign_in_email=${email}; expires="Wed, 23 May 2018 09:16:33 GMT"`)
+        let sessionId = Math.random() * 1000000
+        sessions[sessionId] = {sign_in_email: email}
+        response.setHeader('Set-Cookie', `sessionId=${sessionId};`)
         response.statusCode = 200
       } else {
         response.statusCode = 401
       }
       response.end()
     })
-  } else {
+  } else if (pathName === '/js/main.js'){
+    //使用ETag
+    let string = fs.readFileSync('./js/main.js', 'utf8')
+    response.setHeader('Content-Type', 'application/javascript; charset=utf8')
+    let fileMd5 = md5(string)
+    response.setHeader('ETag', fileMd5)
+    if(request.headers['if-none-match'] === fileMd5){
+      //main.js没有更新，没有响应体
+      response.statusCode = 304
+    } else{
+      //有更新，再把新的内容返回
+      response.write(string)
+    }
+    response.end()
+  } else if (pathName === '/css/default.css'){
+    //使用Cache-Control
+    let string = fs.readFileSync('./css/default.css', 'utf8')
+    response.setHeader('Content-Type', 'text/css; charset=utf8')
+    response.setHeader('Cache-Control', 'max-age=300000000')//10年内使用缓存，不向服务器发送请求
+    // response.setHeader('Expires', 'Thu, 24 May 2018 02:18:30 GMT')
+    response.write(string)
+    response.end()
+  }else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/plain;charset=utf-8')
     response.write('找不到对应路径')
